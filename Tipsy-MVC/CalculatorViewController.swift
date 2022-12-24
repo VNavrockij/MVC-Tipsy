a69ad1fed98ccc143364e749b956d474aba46a00
VNavrockij <vitalik.nav@gmail.com>
2022-12-24 14:27:20 +0200

    add: outlets and actions

---
+++ Tipsy-MVC/CalculatorViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Tipsy-MVC
+//
+//  Created by Vitalii Navrotskyi on 20.12.2022.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+    
+    
+}
+
